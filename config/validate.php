4e3580204abe34e65d177ac62568a46876de1b93
Mai Huy Thuong <thuongmaihuy.demo@gmail.com>
2020-09-17 12:06:55 +0700

    add comment file config

--- config/validate.php
+++ config/validate.php
@@ -1,11 +1,18 @@
 <?php
 return [
+    /**
+     * set of functions to be validated
+     */
     'handle_validate' => [
         'name_file' => true,
         'input_html' => true,
         'input_string' => true,
     ],
 
+    /**
+     * @secction: name_file
+     * list of input names to be validated
+     */
     'list_file_name' => [
         'file',
         'attach_file',
@@ -15,6 +22,10 @@ return [
         'attach_thumbnail'
     ],
 
+    /**
+     * @secction: name_file
+     * list of accepted file extensions
+     */
     'white_list_extension_file' => [
         '.png',
         '.xls',
@@ -26,14 +37,26 @@ return [
         '.mov'
     ],
 
+    /**
+     * @secction: input_html
+     * Regex chain to check
+     */
     'regex_html' => [
         '/<[^>]*>/m'
     ],
 
+    /**
+     * @secction: input_html
+     * Name list of contents will not check
+     */
     'expect_name_html' => [
         'token'
     ],
 
+    /**
+     * @secction: input_string
+     * a list of unacceptable characters at the beginning of each message
+     */
     'ARRAY_REPLACE' => [
         '@',
         '=',
@@ -41,6 +64,9 @@ return [
         '-'
      ],
 
+    /**
+     * list message return
+     */
     'messages' => [
         'validate_name_file' => 'Định dạng file không hợp lệ',
         'validate_input' => 'Có chứa các ký tự bắt đầu không được cho phép (+, -, =, @)',
